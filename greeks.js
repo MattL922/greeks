72af3581b7e335692bbd8697ec1f0825018b5dac
MattL922 <mattloppatto@gmail.com>
2014-11-23 14:12:38 -0500

    added calculations for theta, vega, and rho for call options

--- greeks.js
+++ greeks.js
@@ -6,6 +6,11 @@
 
  var bs = require("black-scholes");
 
+ function _phiPrime(x)
+ {
+   return Math.pow(Math.E, -1 * Math.pow(x, 2) / 2) / Math.sqrt(2 * Math.PI);
+ }
+
  function callDelta(s, k, t, v, r)
  {
    var w = bs.getW(s, k, t, v, r);
@@ -16,3 +21,19 @@
  {
    return 1 - callDelta(s, k, t, v, r);
  }
+
+function callRho(s, k, t, v, r)
+{
+  return k * t * Math.pow(Math.E, -1 * r * t) * bs.stdNormCDF(bs.getW(s, k, t, v, r) - v * Math.sqrt(t));
+}
+
+function callVega(s, k, t, v, r)
+{
+  return s * Math.sqrt(t) * _phiPrime(bs.getW(s, k, t, v, r));
+}
+
+function callTheta(s, k, t, v, r)
+{
+  var w = bs.getW(s, k, t, v, r);
+  return v * s * _phiPrime(w) / (2 * Math.sqrt(t)) + k * r * Math.pow(Math.E, -1 * r * t) * bs.stdNormCDF(w - v * Math.sqrt(t));
+}
